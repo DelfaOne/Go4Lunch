a166e9393c96ee3dce24cc99ddff88b8079d0532
Nino <anthony@delcey.fr>
2021-12-21 21:20:33 +0100

    Update permission code

--- app/src/main/java/com/fadel/go4lunch/HostActivityViewModel.kt
+++ app/src/main/java/com/fadel/go4lunch/HostActivityViewModel.kt
@@ -1,14 +1,24 @@
 package com.fadel.go4lunch
 
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class HostActivityViewModel @Inject constructor(
+    private val context: Context,
     private val permissionRepository: PermissionRepository
 ){
 
     fun onResume() {
+        val grantedPermissions = getPermissionList().filter { permission ->
+            ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        }
 
+        permissionRepository.updatePermission(grantedPermissions)
     }
+
+    private fun getPermissionList() = listOf(PERMISSION1, PERMISSION2)
 }
